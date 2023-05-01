6ecf4cefc156fcd42188c1df6b3c0707839fe30f
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-05-02 02:11:59 +0600

    add cors

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express();
 const port = 5000;
 const allData = require('./data.json')
+const cors = require('cors')
 
+
+app.use(cors());
 app.get('/', (req, res) =>{
     res.send('Hello World')
 })
