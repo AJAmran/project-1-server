27d42e6549bcf601a91d046018d043a4404d47c2
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-05-02 02:03:16 +0600

    send all data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express();
 const port = 5000;
+const allData = require('./data.json')
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
+})
+
+app.get('/alldata', (req, res) =>{
+    res.send(allData)
 })
 
 app.listen(port, () =>{
